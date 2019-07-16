f0d256dd5e24ab7b299ea4ae4b4c7a015e906703
Francis Gagnaire <francis.gagnaire@gmail.com>
2019-07-15 22:06:27 -0500

    move some stuff from the other repo

---
+++ docker-run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+
+DOCKER_WORKSPACE=${DOCKER_WORKSPACE:=${HOME}/workspace}
+
+
+set +e
+docker run \
+    -it \
+    --rm \
+    --net=host \
+    -e DISPLAY \
+    -e SHELL \
+    -e LOCAL_USER_ID=$(id -u)\
+    -e LOCAL_USER_HOME=${HOME} \
+    -e LOCAL_USER_NAME=${USERNAME} \
+    -v ${HOME}/.Xauthority:${HOME}/.Xauthority \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v /private:/private \
+    -w ${PWD} \
+    -v ${DOCKER_WORKSPACE}:${DOCKER_WORKSPACE} \
+    -v /tmp:/tmp \
+    -v ${HOME}/Downloads:${HOME}/Downloads \
+    $@
+
+
+
