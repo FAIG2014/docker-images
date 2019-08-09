8e994c89306ea8d85fb1cd622905d98e0c47228f
Francis Gagnaire <francis.gagnaire@gmail.com>
2019-08-08 21:59:21 -0500

    add docker image fedora with xilinx vivado

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -9,4 +9,7 @@ docker build -t fedora31-modelsim fedora-modelsim
 docker build -t fedora31-fpga_simu fedora-fpga_simu
 
 
+
+docker build -t fedora31-xilinx_vivado fedora-xilinx_vivado
+
 docker build -t fedora31-lattice_diamond fedora-lattice_diamond
