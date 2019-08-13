0fb01e98fef120b6963adc6db27c29726512f137
Francis Gagnaire <francis.gagnaire@gmail.com>
2019-08-13 17:04:51 -0500

    better logger

--- fpga/sources/rtl/misc/logs.svh
+++ fpga/sources/rtl/misc/logs.svh
@@ -1,5 +1,9 @@
 
 
+`ifndef LOGS_SVH
+`define LOGS_SVH
+
+
 `ifdef SIMULATION
 
     /*
@@ -9,41 +13,52 @@
 
     `define DELIM
 
-    `ifndef LOG_INFO
-        `define LOG_INFO(p0, p1=ELIM, p2=ELIM) \
-            `ifdef D``p1 \
-                                logger_pkg::log_info($psprintf(p0), $sformatf("%m"), `__FILE__, `__LINE__); \
+    `define LOG_INFO(p0, p1=ELIM, p2=ELIM) \
+        `ifdef D``p1 \
+                            logger_pkg::log_info($sformatf(p0), $sformatf("%m"), `__FILE__, `__LINE__); \
+        `else \
+            `ifdef D``p2 \
+                            logger_pkg::log_info($sformatf(p0, p1), $sformatf("%m"), `__FILE__, `__LINE__); \
             `else \
-                `ifdef D``p2 \
-                                logger_pkg::log_info($psprintf(p0, p1), $sformatf("%m"), `__FILE__, `__LINE__); \
-                `else \
-                                logger_pkg::log_info($psprintf(p0, p1, p2), $sformatf("%m"), `__FILE__, `__LINE__); \
-                `endif \
-            `endif // !`ifdef D
-    `endif
-/*
-    `ifndef LOG_INFO
-    `define LOG_INFO(msg) \
-            $display("%c[0;37mINFO:     [%g] %s%c[0m", 27, $time, msg, 27)
-    `endif
-*/
-
-    `ifndef LOG_INFO_GREEN
-    `define LOG_INFO_GREEN(msg) \
-            $display("%c[0;32mINFO:     [%g] %s%c[0m", 27, $time, msg, 27)
-    `endif
+                            logger_pkg::log_info($sformatf(p0, p1, p2), $sformatf("%m"), `__FILE__, `__LINE__); \
+            `endif \
+        `endif // !`ifdef D
 
 
+    `define LOG_INFO_GREEN(p0, p1=ELIM, p2=ELIM) \
+        `ifdef D``p1 \
+                            logger_pkg::log_info_green($sformatf(p0), $sformatf("%m"), `__FILE__, `__LINE__); \
+        `else \
+            `ifdef D``p2 \
+                            logger_pkg::log_info_green($sformatf(p0, p1), $sformatf("%m"), `__FILE__, `__LINE__); \
+            `else \
+                            logger_pkg::log_info_green($sformatf(p0, p1, p2), $sformatf("%m"), `__FILE__, `__LINE__); \
+            `endif \
+        `endif // !`ifdef D
 
-    `ifndef LOG_WARNING
-    `define LOG_WARNING(msg) \
-            $display("%c[1;33mWARNING:  [%g] %s%c[0m", 27, $time, msg, 27);
-    `endif
 
-    `ifndef LOG_ERROR
-    `define LOG_ERROR(msg) \
-            $display("%c[1;31mERROR:    [%g] %s%c[0m", 27, $time, msg, 27);
-    `endif
+    `define LOG_WARNING(p0, p1=ELIM, p2=ELIM) \
+        `ifdef D``p1 \
+                            logger_pkg::log_warning($sformatf(p0), $sformatf("%m"), `__FILE__, `__LINE__); \
+        `else \
+            `ifdef D``p2 \
+                            logger_pkg::log_warning($sformatf(p0, p1), $sformatf("%m"), `__FILE__, `__LINE__); \
+            `else \
+                            logger_pkg::log_warning($sformatf(p0, p1, p2), $sformatf("%m"), `__FILE__, `__LINE__); \
+            `endif \
+        `endif // !`ifdef D
+
+
+    `define LOG_ERROR(p0, p1=ELIM, p2=ELIM) \
+        `ifdef D``p1 \
+                            logger_pkg::log_error($sformatf(p0), $sformatf("%m"), `__FILE__, `__LINE__); \
+        `else \
+            `ifdef D``p2 \
+                            logger_pkg::log_error($sformatf(p0, p1), $sformatf("%m"), `__FILE__, `__LINE__); \
+            `else \
+                            logger_pkg::log_error($sformatf(p0, p1, p2), $sformatf("%m"), `__FILE__, `__LINE__); \
+            `endif \
+        `endif // !`ifdef D
 
 `else
     `define LOG_INFO(msg)
@@ -51,3 +66,6 @@
     `define LOG_WARNING(msg)
     `define LOG_ERROR(msg)
 `endif
+
+
+`endif // `ifndef LOGS_SVH
