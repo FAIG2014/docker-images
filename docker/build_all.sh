d6c31ad63ed77b4ef9a3060b189d981486d43891
Francis Gagnaire <francis.gagnaire@gmail.com>
2019-08-14 22:01:17 -0500

    add tiny FPGA for fun

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -9,7 +9,11 @@ docker build -t fedora31-modelsim fedora-modelsim
 docker build -t fedora31-fpga_simu fedora-fpga_simu
 
 
-
+#xilinx
 docker build -t fedora31-xilinx_vivado fedora-xilinx_vivado
 
+# lattice
 docker build -t fedora31-lattice_diamond fedora-lattice_diamond
+
+#intel FPGA
+docker build -t fedora31-intel_quartus fedora-intel_quartus
